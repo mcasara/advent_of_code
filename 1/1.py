6c4f0f9b60713d5b1647f52293a59f254de5d38a
mcasara <maxime.casara@gmail.com>
2022-12-17 14:47:07 +0100

    solution

--- 1/1.py
+++ 1/1.py
@@ -1,8 +1,37 @@
-def read_input(file):
+def read_input(file, type):
+    content = []
     with open(file,'r') as f:
-        content = f.readlines()
+        if type == 'float':
+            for line in f:
+                try:
+                    content.append(float(line.strip('\n')))
+                except:
+                    content.append('null')
+        elif type == 'int':
+            for line in f:
+                try:
+                    content.append(int(line.strip('\n')))
+                except:
+                    content.append('null')
+        else:
+            for line in f:
+                content.append(line.strip('\n'))
     return(content)
-def main():
-    print (read_input("input_1.txt"))
 
-main()
+def sum_list_with_null(l):
+    sum_dict = {0:0}
+    i=0
+    for amount in l:
+        if amount == 'null':
+            i+=1
+            sum_dict[i] = float(0)
+        else:
+            sum_dict[i]+=amount
+    return sum_dict
+
+def main():
+    liste= sum_list_with_null(read_input("1/input_1.txt", 'float'))
+    return(print( max(liste.items(), key=lambda k: k[1])))
+
+if __name__ == "__main__" :
+    main()
