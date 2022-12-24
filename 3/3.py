0718bd86f8f8529d26aae4e0eaf0617917b0b6e1
mcasara <maxime.casara@gmail.com>
2022-12-24 23:21:02 +0100

    3b

--- 3/3.py
+++ 3/3.py
@@ -22,17 +22,34 @@ def intersection(input_1, input_2):
     return list(set(input_1).intersection(set(input_2)))[0]
 
 
+def intersection_2(input_1, input_2, input_3):
+    intersect_1 = set(input_1).intersection(set(input_2))
+    intersect_2 = intersect_1.intersection(set(input_3))
+    return list(intersect_2)[0]
+
+
 def transform_input(list_input):
     result = []
     for k in list_input:
         a, b = halve_list(k)
-        result.append(points((intersection(a, b))[0]))
+        result.append(points((intersection(a, b))))
+    return result
+
+
+def transform_input_2(list_input):
+    result = []
+    i = 0
+    while i < len(list_input)-2:
+        a, b, c = list_input[i], list_input[i + 1], list_input[i + 2]
+        result.append(points((intersection_2(a, b, c))))
+        i += 3
     return result
 
 
 def main():
     file = read_input("input_3.txt")
-    return print(sum(transform_input(file)))
+    return print(f"Part 1:{sum(transform_input(file))}",
+                 f"Part 2: {sum(transform_input_2(file))}")
 
 
 if __name__ == "__main__":
