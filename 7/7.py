f5165b4f49aeaf662c708b23e7c2f3c549b08fad
mcasara <maxime.casara@gmail.com>
2022-12-27 02:33:37 +0100

    7b

--- 7/7.py
+++ 7/7.py
@@ -6,9 +6,25 @@ def read_input(file):
     return content
 
 
+def read_all_directories(input):
+    current_dir = ['/']
+    directories_tree = dict()
+    for k in input:
+        if k == '$ cd ..':
+            current_dir.pop(-1)
+        if k[:4] == '$ cd' and k[5:] != '..':
+            new_dir = k[5:]
+            try:
+                directories_tree[current_dir[-1]] = directories_tree[current_dir[-1]] + ', ' + new_dir
+            except:
+                directories_tree[current_dir[-1]] = new_dir
+            current_dir.append(new_dir)
+    return directories_tree
+
+
 def main():
     file = read_input("input_7.txt")
-    return print(file)
+    return print(read_all_directories(file[1:]))
     # return print(f"Part 1: {solve(file[0], 4)}"
     #              f"\nPart 2: {solve(file[0], 14)}")
 
