e9b4ef17e76c70c7a10078abf9a0c92803bda593
mcasara <maxime.casara@gmail.com>
2022-12-27 18:44:50 +0100

    7c

--- 7/7.py
+++ 7/7.py
@@ -1,3 +1,6 @@
+import time
+
+
 def read_input(file):
     content = []
     with open(file, 'r') as f:
@@ -7,27 +10,55 @@ def read_input(file):
 
 
 def read_all_directories(input):
-    current_dir = ['/']
+    current_dir = ['root']
     directories_tree = dict()
-    for k in input:
-        if k == '$ cd ..':
+    directories_size = dict({'root':0})
+    k = 0
+    while k < len(input):
+        if input[k] == '$ cd ..':
             current_dir.pop(-1)
-        if k[:4] == '$ cd' and k[5:] != '..':
-            new_dir = k[5:]
+        if input[k][:4] == '$ cd' and input[k][5:] != '..':
+            new_dir = input[k][5:]
+            string_dir = '/'.join(current_dir)
             try:
-                directories_tree[current_dir[-1]] = directories_tree[current_dir[-1]] + ', ' + new_dir
+                directories_tree[string_dir] = directories_tree[
+                                                   string_dir] + ', ' + '/'.join(
+                    current_dir + [new_dir])
             except:
-                directories_tree[current_dir[-1]] = new_dir
+                directories_tree[string_dir] = '/'.join(current_dir + [new_dir])
             current_dir.append(new_dir)
-    return directories_tree
+            string_new_dir = '/'.join(current_dir)
+            if string_new_dir not in directories_size:
+                directories_size[string_new_dir] = 0
+            k += 1
+            while k < len(input) and input[k][:4] != '$ cd':
+                if input[k][0] != '$' and input[k][:3] != 'dir':
+                    directories_size[string_new_dir] += int(input[k].split(' ')[0])
+                k += 1
+            k -= 1
+        k += 1
+    list_dir = [k.split('/') for k in list(directories_tree.keys())]
+    length = 0
+    for k in list_dir:
+        if len(k) > length:
+            length = len(k)
+    while length > -1:
+        for items in list_dir:
+            directory = '/'.join(items)
+            if len(items) == length:
+                directories_size[directory] += sum(
+                    [directories_size[k] for k in directories_tree[directory].split(', ')])
+        length -= 1
+    return sum([k if k < 100000 else 0 for k in list(directories_size.values())])
 
 
 def main():
     file = read_input("input_7.txt")
-    return print(read_all_directories(file[1:]))
-    # return print(f"Part 1: {solve(file[0], 4)}"
+    return print(f"Part 1: {read_all_directories(file[1:])}")
     #              f"\nPart 2: {solve(file[0], 14)}")
 
 
 if __name__ == "__main__":
+    start_time = time.process_time()
     main()
+    print(time.process_time() - start_time, "seconds")
